337a8c108c9bb5824ff771fd838ddde859577df5
xiao mingg <minhnguyen47431@gmail.com>
2026-04-09 11:21:01 +0700

    a

--- models.py
+++
@@ -1,8 +0,0 @@
-import torch
-from . import net
-
-def get_model(name, checkpoint):
-    model = net.build_model(name)
-    state_dict = torch.load(checkpoint)
-    model.load_state_dict(state_dict)
-    return model
