afc18641683cf93be9b61ca0a19a0799e1d093a1
xiao ming <minhnguyen47431@gmail.com>
2026-04-12 00:00:28 +0700

    fix

---
+++ trainer.py
@@ -0,0 +1,53 @@
+import torch
+from .utils import load_checkpoint
+from torch.cuda.amp import autocast, GradScaler
+from tqdm import tqdm
+import numpy as np
+
+class Trainer:
+    def __init__(self, dataloader, model, head, optimizer, criterion, device, epochs):
+        self.dataloader = dataloader
+        self.model = model
+        self.head = head
+        self.optimizer = optimizer
+        self.criterion = criterion
+        self.device = device
+        self.epoch = 0
+        self.epochs = epochs
+        
+            
+    def _load_checkpoint(self, file):
+        self.epoch = load_checkpoint(file, self.model, self.head, self.optimizer)
+        
+        
+    def train(self):
+        self.model.train()
+        scaler = GradScaler()
+        train_losses = []
+        
+        for it in range(self.epoch, self.epochs):
+            
+            pbar = tqdm(self.dataloader, desc=f"Epoch {it+1}/{self.epochs}")
+            for images, labels in pbar:
+                
+                self.optimizer.zero_grad()
+                
+                with torch.autocast(device_type=self.device, dtype=torch.float16):
+                    embedings, norm = self.model(images)
+                    cos_theta = self.head(embedings, norm, labels)
+                    loss = self.criterion(cos_theta, labels)
+                    
+                train_losses.append(loss.item())
+                    
+                scaler.scale(loss).backward()
+                scaler.step(self.optimizer)
+                scaler.update()
+                
+            # update tqdm
+            pbar.set_postfix({
+                "loss": f"{np.mean(train_losses):.4f}",
+                "lr": self.optimizer.param_groups[0]["lr"]
+            })
+        
+        
+    
--- utils.py
+++ utils.py
@@ -27,7 +27,7 @@ def fuse_features_with_norm(stacked_embeddings, stacked_norms):
     return fused, fused_norm
  
  
-class SaveCheckPoint:
+class CheckPoint:
     def __init__(self, model, head, optimizer):
         self.model = model
         self.head = head
@@ -45,21 +45,22 @@ class SaveCheckPoint:
                 'epoch': epoch
             })
             print("\nSave checkpoint successfully!\n")
-        
-    def load_checkpoint(self, file, device='gpu'):
-        if os.path.isfile(file):
-            statedict = torch.load(file, weights_only=False, map_location=device)
-        
-            self.model.load_state_dict(statedict['model_statedict'])
-            self.head.load_state_dict(statedict['head_statedict'])
-            self.optimizer.load_state_dict(statedict['optimizer_statedict'])
-            epoch = statedict.get('epoch')
-
-            print(f"Successfully load model statedict with epoch {epoch}.\n")
             
-            return epoch
         
-        return -1
+def load_checkpoint(file, model, head, optimizer, device='gpu'):
+    if os.path.isfile(file):
+        statedict = torch.load(file, weights_only=False, map_location=device)
+    
+        model.load_state_dict(statedict['model_statedict'])
+        head.load_state_dict(statedict['head_statedict'])
+        optimizer.load_state_dict(statedict['optimizer_statedict'])
+        epoch = statedict.get('epoch')
+
+        print(f"Successfully load model statedict with epoch {epoch}.\n")
+        
+        return epoch
+    
+    return 0
 
 
 def split_parameters(module):
@@ -72,3 +73,57 @@ def split_parameters(module):
             params_decay.extend([*m.parameters()])
     assert len(list(module.parameters())) == len(params_decay) + len(params_no_decay)
     return params_decay, params_no_decay
+
+
+
+
+
+
+from .data import CustomImageFolderDataset
+from torch.utils.data import DataLoader
+from .head import AdaFace
+from . import net
+
+def get_loader(data_path, transforms, batch_size, shuffle=False,
+               low_res_augmentation_prob=0.0, crop_augmentation_prob=0.0, photometric_augmentation_prob=0.0):
+    dataset = CustomImageFolderDataset(data_path,
+                                        transform=transforms,
+                                        low_res_augmentation_prob=low_res_augmentation_prob,
+                                        crop_augmentation_prob=crop_augmentation_prob,
+                                        photometric_augmentation_prob=photometric_augmentation_prob
+                                        )
+    loader = DataLoader(
+        dataset,
+        batch_size=batch_size,
+        shuffle=shuffle,
+        num_workers=8,
+        pin_memory=True,
+        persistent_workers=True,
+        prefetch_factor=4
+    )
+    return loader
+
+def get_model(model_name, device):
+    model = net.build_model(model_name)
+    model.to(device)
+    return model
+
+
+def get_head(device, embedding_size=512, classnum=8631, m=0.4, h=0.333, s=64, t_alpha=0.99):
+    head = AdaFace(embedding_size=embedding_size, classnum=classnum, m=m, h=h, s=s, t_alpha=t_alpha)
+    head.to(device)
+    return head
+
+
+def get_optimizer(model, head, lr):
+    paras_wo_bn, paras_only_bn = split_parameters(model)
+
+    optimizer = torch.optim.SGD([{
+                'params': paras_wo_bn + [head.kernel],
+                'weight_decay': 1e-4
+            }, {
+                'params': paras_only_bn
+            }], lr=lr, momentum=0.9)
+    
+    return optimizer
+
